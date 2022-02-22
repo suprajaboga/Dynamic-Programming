70a84734dd11a6ae7bb1d72937c89136691edca7
suprajaboga <97597175+suprajaboga@users.noreply.github.com>
2022-02-22 12:53:40 +0530

    File updated

--- maxorminsumpathtriangle/maxorminsumpathtriangle.java
+++ maxorminsumpathtriangle/maxorminsumpathtriangle.java
@@ -6,64 +6,62 @@ package maxorminsumpathtriangle;
 import java.util.*;
 
 //Recursion
-//public class maxorminsumpathtriangle 
-//{
-//	public static int func(int i,int j,int arr[][])
-//	{
-//		if(j<0||j>=arr[0].length)
-//			return Integer.MIN_VALUE;
-//		if(i==0&&j<arr[0].length)
-//			return arr[i][j];
-//		int up = arr[i][j] + func(i-1,j,arr);
-//		int leftdiagonal = arr[i][j] + func(i-1,j-1,arr);
-//		int rightdiagonal = arr[i][j] + func(i-1,j+1,arr);
-//		return Math.max(up, Math.max(leftdiagonal, rightdiagonal));
-//	}
-//
-//	public static void main(String[] args) 
-//	{
-//		int row=3,col=3;
-//		int arr[][];
-//		arr= new int[][]{{2,1,3},{6,5,4},{7,8,9}};
-//		int maxi=Integer.MIN_VALUE;
-//		for(int j=0;j<col;j++)
-//			maxi=Math.max(maxi, func(row-1,j,arr));
-//		System.out.println("Maximum sum: "+ maxi);
-//	}
-//}
+public class maxorminsumpathtriangle 
+{
+	public static int func(int i,int j,int arr[][])
+	{
+		if(j<0||j>=arr[0].length)
+			return Integer.MIN_VALUE;
+		if(i==0&&j<arr[0].length)
+			return arr[i][j];
+		int up = arr[i][j] + func(i-1,j,arr);
+		int leftdiagonal = arr[i][j] + func(i-1,j-1,arr);
+		int rightdiagonal = arr[i][j] + func(i-1,j+1,arr);
+		return Math.max(up, Math.max(leftdiagonal, rightdiagonal));
+	}
+	public static void main(String[] args) 
+	{
+		int row=3,col=3;
+		int arr[][];
+		arr= new int[][]{{2,1,3},{6,5,4},{7,8,9}};
+		int maxi=Integer.MIN_VALUE;
+		for(int j=0;j<col;j++)
+			maxi=Math.max(maxi, func(row-1,j,arr));
+		System.out.println("Maximum sum: "+ maxi);
+	}
+}
 
 
 //Memoization
-//public class maxorminsumpathtriangle 
-//{
-//	public static int func(int i,int j,int arr[][],int dp[][])
-//	{
-//		if(j<0||j>=arr[0].length)
-//			return Integer.MIN_VALUE;
-//		if(i==0&&j<arr[0].length)
-//			return arr[i][j];
-//		if(dp[i][j]!=-1)
-//			return dp[i][j];
-//		int up = arr[i][j] + func(i-1,j,arr,dp);
-//		int leftdiagonal = arr[i][j] + func(i-1,j-1,arr,dp);
-//		int rightdiagonal = arr[i][j] + func(i-1,j+1,arr,dp);
-//		return dp[i][j]=Math.max(up, Math.max(leftdiagonal, rightdiagonal));
-//	}
-//
-//	public static void main(String[] args) 
-//	{
-//		int row=3,col=3;
-//		int arr[][];
-//		arr= new int[][]{{2,1,3},{6,5,4},{7,8,9}};
-//		int dp[][]=new int[row][col];
-//		for(int[] ele:dp)
-//			Arrays.fill(ele,-1);
-//		int maxi=Integer.MIN_VALUE;
-//		for(int j=0;j<col;j++)
-//			maxi=Math.max(maxi, func(row-1,j,arr,dp));
-//		System.out.println("Maximum sum: "+ maxi);
-//	}
-//}
+public class maxorminsumpathtriangle 
+{
+	public static int func(int i,int j,int arr[][],int dp[][])
+	{
+		if(j<0||j>=arr[0].length)
+			return Integer.MIN_VALUE;
+		if(i==0&&j<arr[0].length)
+			return arr[i][j];
+		if(dp[i][j]!=-1)
+			return dp[i][j];
+		int up = arr[i][j] + func(i-1,j,arr,dp);
+		int leftdiagonal = arr[i][j] + func(i-1,j-1,arr,dp);
+		int rightdiagonal = arr[i][j] + func(i-1,j+1,arr,dp);
+		return dp[i][j]=Math.max(up, Math.max(leftdiagonal, rightdiagonal));
+	}
+	public static void main(String[] args) 
+	{
+		int row=3,col=3;
+		int arr[][];
+		arr= new int[][]{{2,1,3},{6,5,4},{7,8,9}};
+		int dp[][]=new int[row][col];
+		for(int[] ele:dp)
+			Arrays.fill(ele,-1);
+		int maxi=Integer.MIN_VALUE;
+		for(int j=0;j<col;j++)
+			maxi=Math.max(maxi, func(row-1,j,arr,dp));
+		System.out.println("Maximum sum: "+ maxi);
+	}
+}
 
 
 //Tabulation
