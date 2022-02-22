1a0ebb5cac986e5f3ca3b299c6ba6d9ccad28772
suprajaboga <97597175+suprajaboga@users.noreply.github.com>
2022-02-22 12:57:37 +0530

    File updated

--- triangle/triangle.java
+++ triangle/triangle.java
@@ -5,77 +5,73 @@ package triangle;
 import java.util.*;
 
 //Recursion  TC: O(2 pow n)  SC: stack space for recursive calls
-//public class triangle
-//{
-//	public static int func(int i,int j,List<List<Integer>> triangle,int n)
-//	{
-//		if(i==n-1)
-//			return triangle.get(n-1).get(j);
-//		int down = triangle.get(i).get(j) + func(i+1,j,triangle,n);
-//		int diagonal =triangle.get(i).get(j) + func(i+1,j+1,triangle,n);
-//		return Math.min(down, diagonal);
-//	}
-//
-//	public static void main(String[] args) 
-//	{
-//		int n=1;
-//		List<List<Integer>> triangle=new ArrayList<>();
-//		Scanner sc=new Scanner(System.in);
-//		for(int i=1;i<=n;i++)
-//		{
-//			List<Integer> row=new ArrayList<>();
-//			for(int j=1;j<=i;j++)
-//			{
-//				System.out.println("Enter element");
-//				row.add(sc.nextInt());
-//			}
-//			triangle.add(row);
-//		}
-//		System.out.println("Minimum path sum: " + func(0,0,triangle,n));
-//	}
-//
-//}
+public class triangle
+{
+	public static int func(int i,int j,List<List<Integer>> triangle,int n)
+	{
+		if(i==n-1)
+			return triangle.get(n-1).get(j);
+		int down = triangle.get(i).get(j) + func(i+1,j,triangle,n);
+		int diagonal =triangle.get(i).get(j) + func(i+1,j+1,triangle,n);
+		return Math.min(down, diagonal);
+	}
+	public static void main(String[] args) 
+	{
+		int n=1;
+		List<List<Integer>> triangle=new ArrayList<>();
+		Scanner sc=new Scanner(System.in);
+		for(int i=1;i<=n;i++)
+		{
+			List<Integer> row=new ArrayList<>();
+			for(int j=1;j<=i;j++)
+			{
+				System.out.println("Enter element");
+				row.add(sc.nextInt());
+			}
+			triangle.add(row);
+		}
+		System.out.println("Minimum path sum: " + func(0,0,triangle,n));
+	}
+}
 
 
 //Memoization		TC: O(n*m), because we have n*m states like we visit 1*2, 2*3 and if that value is already present in dp, we pick
 //					from there  		 SC: recursion stack space length + O(n*m) for DP array
-//public class triangle
-//{
-//	public static int func(int i,int j,List<List<Integer>> triangle,int n,int dp[][])
-//	{
-//		if(i==n-1)
-//			return triangle.get(n-1).get(j);
-//		if(dp[i][j]!=-1)
-//			return dp[i][j];
-//		int down = triangle.get(i).get(j) + func(i+1,j,triangle,n,dp);
-//		int diagonal =triangle.get(i).get(j) + func(i+1,j+1,triangle,n,dp);
-//		return dp[i][j]=Math.min(down, diagonal);
-//	}
-//
-//	public static void main(String[] args) 
-//	{
-//		int n=4;
-//		List<List<Integer>> triangle=new ArrayList<>();
-//		Scanner sc=new Scanner(System.in);
-//		for(int i=1;i<=n;i++)
-//		{
-//			List<Integer> row=new ArrayList<>();
-//			for(int j=1;j<=i;j++)
-//			{
-//				System.out.println("Enter element");
-//				row.add(sc.nextInt());
-//			}
-//			triangle.add(row);
-//		}
-//		int row=triangle.size();
-//		int col=triangle.get(row-1).size();
-//		int dp[][]=new int[row][col];
-//		for(int[] ele:dp)
-//			Arrays.fill(ele,-1);
-//		System.out.println("Minimum path sum: " + func(0,0,triangle,n,dp));
-//	}
-//
-//}
+public class triangle
+{
+	public static int func(int i,int j,List<List<Integer>> triangle,int n,int dp[][])
+	{
+		if(i==n-1)
+			return triangle.get(n-1).get(j);
+		if(dp[i][j]!=-1)
+			return dp[i][j];
+		int down = triangle.get(i).get(j) + func(i+1,j,triangle,n,dp);
+		int diagonal =triangle.get(i).get(j) + func(i+1,j+1,triangle,n,dp);
+		return dp[i][j]=Math.min(down, diagonal);
+	}
+	public static void main(String[] args) 
+	{
+		int n=4;
+		List<List<Integer>> triangle=new ArrayList<>();
+		Scanner sc=new Scanner(System.in);
+		for(int i=1;i<=n;i++)
+		{
+			List<Integer> row=new ArrayList<>();
+			for(int j=1;j<=i;j++)
+			{
+				System.out.println("Enter element");
+				row.add(sc.nextInt());
+			}
+			triangle.add(row);
+		}
+		int row=triangle.size();
+		int col=triangle.get(row-1).size();
+		int dp[][]=new int[row][col];
+		for(int[] ele:dp)
+			Arrays.fill(ele,-1);
+		System.out.println("Minimum path sum: " + func(0,0,triangle,n,dp));
+	}
+}
 
 
 //Tabulation		TC: O(n*m)    SC: O(n*m) for DP array, no recursion stack space
@@ -116,5 +112,4 @@ public class triangle
 		}
 		System.out.println("Minimum path sum: " + dp[0][0]);
 	}
-
 }
