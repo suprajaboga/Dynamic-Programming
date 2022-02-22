62a4d6b122a8a3702b75fc26b3c75956fd826cef
suprajaboga <97597175+suprajaboga@users.noreply.github.com>
2022-02-22 12:52:50 +0530

    File updated

--- maximumsum/maximumsum.java
+++ maximumsum/maximumsum.java
@@ -5,77 +5,75 @@ package maximumsum;
 import java.util.Arrays;
 
 //Recursion
-//public class maximumsum
-//{
-//	public static int func(int index,int arr[])
-//	{
-//		if(index==0)		//if ind=0 means, we didn't pick index=1 ele, and consider this ind[0] ele as we want to maximise the sum
-//			return arr[index];
-//		if(index<0)
-//			return 0;
-//		int pick=arr[index]+func(index-2,arr);
-//		int notpick=0+func(index-1,arr);
-//		return Math.max(pick,notpick);
-//	}
-//
-//	public static void main(String[] args) 
-//	{
-//		int arr[]= {2,1,4,9};
-//		int n=arr.length;
-//		System.out.println("Maximum sum is: "+func(n-1,arr));
-//	}
-//}
+public class maximumsum
+{
+	public static int func(int index,int arr[])
+	{
+		if(index==0)		//if ind=0 means, we didn't pick index=1 ele, and consider this ind[0] ele as we want to maximise the sum
+			return arr[index];
+		if(index<0)
+			return 0;
+		int pick=arr[index]+func(index-2,arr);
+		int notpick=0+func(index-1,arr);
+		return Math.max(pick,notpick);
+	}
+	public static void main(String[] args) 
+	{
+		int arr[]= {2,1,4,9};
+		int n=arr.length;
+		System.out.println("Maximum sum is: "+func(n-1,arr));
+	}
+}
 
 
 //Memoization
-//public class maximumsum
-//{
-//	public static int func(int index,int arr[],int dp[])
-//	{
-//		if(index==0)
-//			return arr[index];
-//		if(index<0)
-//			return 0;
-//		if(dp[index]!=-1)
-//			return dp[index];
-//		int pick=arr[index]+func(index-2,arr,dp);
-//	    int notpick=0+func(index-1,arr,dp);
-//		return dp[index]=Math.max(pick,notpick);
-//	}
-//
-//	public static void main(String[] args) 
-//	{
-//		int arr[]= {2,1,4,9};
-//		int n=arr.length;
-//		int dp[]=new int[n+1];
-//		Arrays.fill(dp, -1);
-//		System.out.println("Maximum sum is: "+func(n-1,arr,dp));
-//	}
-//}
+public class maximumsum
+{
+	public static int func(int index,int arr[],int dp[])
+	{
+		if(index==0)
+			return arr[index];
+		if(index<0)
+			return 0;
+		if(dp[index]!=-1)
+			return dp[index];
+		int pick=arr[index]+func(index-2,arr,dp);
+	    int notpick=0+func(index-1,arr,dp);
+		return dp[index]=Math.max(pick,notpick);
+	}
+	public static void main(String[] args) 
+	{
+		int arr[]= {2,1,4,9};
+		int n=arr.length;
+		int dp[]=new int[n+1];
+		Arrays.fill(dp, -1);
+		System.out.println("Maximum sum is: "+func(n-1,arr,dp));
+	}
+}
 
 
 //Tabulation
-//public class maximumsum
-//{
-//	public static void main(String[] args) 
-//	{
-//		int arr[]= {2,1,4,9};
-//		int n=arr.length;
-//		int dp[]=new int[n];
-//		int neg=0;
-//		Arrays.fill(dp, -1);
-//		dp[0]=arr[0];
-//		for(int index=1;index<n;index++)
-//		{
-//			int pick=arr[index];
-//			if(index>1)
-//				pick+=dp[index-2];
-//			int notpick=0+dp[index-1];
-//			dp[index]=Math.max(pick,notpick);
-//		}
-//		System.out.println("Maximum sum is: "+dp[n-1]);
-//	}
-//}
+public class maximumsum
+{
+	public static void main(String[] args) 
+	{
+		int arr[]= {2,1,4,9};
+		int n=arr.length;
+		int dp[]=new int[n];
+		int neg=0;
+		Arrays.fill(dp, -1);
+		dp[0]=arr[0];
+		for(int index=1;index<n;index++)
+		{
+			int pick=arr[index];
+			if(index>1)
+				pick+=dp[index-2];
+			int notpick=0+dp[index-1];
+			dp[index]=Math.max(pick,notpick);
+		}
+		System.out.println("Maximum sum is: "+dp[n-1]);
+	}
+}
 
 
 //space optimisation
