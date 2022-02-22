70aaa02bb9c58f9861e7599bc2cabe28f9f8af4c
suprajaboga <97597175+suprajaboga@users.noreply.github.com>
2022-02-22 12:50:05 +0530

    File updated

--- fibonacci/fibonacci.java
+++ fibonacci/fibonacci.java
@@ -8,44 +8,45 @@ import java.util.*;
 //Memoization
 //Time Complexity: O(N), The overlapping subproblems will return the answer in constant time O(1). Therefore the total number of new subproblems we solve is ‘n’. Hence total time complexity is O(N).
 //Space Complexity: O(N), We are using a recursion stack space(O(N)) and an array (again O(N)). Therefore total space complexity will be O(N) + O(N) ≈ O(N)
-//public class fibonacci
-//{
-//	static int func(int n,int dp[])
-//	{
-//		if(n<=1)
-//			return n;
-//		if(dp[n]!=-1)
-//			return dp[n];
-//		dp[n]=func(n-1,dp)+func(n-2,dp);
-//		return dp[n];
-//	}
-//	public static void main(String[] args) 
-//	{
-//		int n=5;
-//		int dp[]=new int[n+1];
-//		Arrays.fill(dp, -1);
-//		System.out.println(func(n,dp));
-//	}
-//}
+public class fibonacci
+{
+	static int func(int n,int dp[])
+	{
+		if(n<=1)
+			return n;
+		if(dp[n]!=-1)
+			return dp[n];
+		dp[n]=func(n-1,dp)+func(n-2,dp);
+		return dp[n];
+	}
+	public static void main(String[] args) 
+	{
+		int n=5;
+		int dp[]=new int[n+1];
+		Arrays.fill(dp, -1);
+		System.out.println(func(n,dp));
+	}
+}
 
 
-//tabulation
+//Tabulation
 //Time Complexity: O(N), We are running a simple iterative loop
 //Space Complexity: O(N), We are using an external array of size ‘n+1’.
-//public class fibonacci
-//{
-//	public static void main(String[] args)
-//	{
-//		int n=5;
-//		int dp[]=new int[n+1];
-//		Arrays.fill(dp,-1);
-//		dp[0]=0;
-//		dp[1]=1;
-//		for(int i=2;i<=n;i++)
-//			dp[i]=dp[i-1]+dp[i-2];
-//		System.out.println(dp[n]);
-//	}
-//}
+public class fibonacci
+{
+	public static void main(String[] args)
+	{
+		int n=5;
+		int dp[]=new int[n+1];
+		Arrays.fill(dp,-1);
+		dp[0]=0;
+		dp[1]=1;
+		for(int i=2;i<=n;i++)
+			dp[i]=dp[i-1]+dp[i-2];
+		System.out.println(dp[n]);
+	}
+}
+
 
 //space optimisation
 //Time Complexity: O(N), We are running a simple iterative loop
