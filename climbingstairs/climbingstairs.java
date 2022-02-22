8c619fbdb0147e4524fa4a3c5d1145b3d2deae6a
suprajaboga <97597175+suprajaboga@users.noreply.github.com>
2022-02-22 12:49:12 +0530

    File updated

--- climbingstairs/climbingstairs.java
+++ climbingstairs/climbingstairs.java
@@ -4,23 +4,24 @@ package climbingstairs;
 
 import java.util.Arrays;
 
-//public class climbingstairs  
-//{
-//	public static int func(int n)
-//	{
-//		if(n<=1)
-//			return 1;
-//		int step1=func(n-1);
-//		int step2=func(n-2);
-//		return step1+step2;
-//	}
-//
-//	public static void main(String[] args)
-//	{
-//		int n=3;
-//		System.out.println(func(n));
-//	}
-//}
+//Recursion
+public class climbingstairs  
+{
+	public static int func(int n)
+	{
+		if(n<=1)
+			return 1;
+		int step1=func(n-1);
+		int step2=func(n-2);
+		return step1+step2;
+	}
+	public static void main(String[] args)
+	{
+		int n=3;
+		System.out.println(func(n));
+	}
+}
+
 
 //Memoization
 public class climbingstairs 
@@ -33,7 +34,6 @@ public class climbingstairs
 			return dp[n];
 		return dp[n]=func(n-1,dp)+func(n-2,dp);
 	}
-
 	public static void main(String[] args)
 	{
 		int n=3;
@@ -43,17 +43,18 @@ public class climbingstairs
 	}
 }
 
+
 //Tabulation
-//public class climbingstairs
-//{
-//	public static void main(String args[])
-//	{
-//		int n=3;
-//		int dp[]=new int[n+1];
-//		Arrays.fill(dp,-1);
-//		dp[0]=dp[1]=1;
-//		for(int i=2;i<=n;i++)
-//			dp[i]=dp[i-1]+dp[i-2];
-//		System.out.println(dp[n]);
-//	}
-//}
+public class climbingstairs
+{
+	public static void main(String args[])
+	{
+		int n=3;
+		int dp[]=new int[n+1];
+		Arrays.fill(dp,-1);
+		dp[0]=dp[1]=1;
+		for(int i=2;i<=n;i++)
+			dp[i]=dp[i-1]+dp[i-2];
+		System.out.println(dp[n]);
+	}
+}
