3e14bc4a3e1d73ebc405eec2ce7e4d94bf1955dd
suprajaboga <97597175+suprajaboga@users.noreply.github.com>
2022-02-22 12:51:20 +0530

    File updated

--- frogjump/frogjump.java
+++ frogjump/frogjump.java
@@ -6,76 +6,74 @@ package frogjump;
 import java.util.Arrays;
 
 //Recursion
-//public class frogjump 
-//{
-//	public static int func(int index,int arr[])
-//	{
-//		if(index==0)		//if frog is at 0th index, then min energy required is 0		
-//			return 0;
-//		int left=func(index-1,arr)+Math.abs(arr[index]-arr[index-1]);
-//		int right=Integer.MAX_VALUE;
-//		if(index>1)			//if we are at index 1, then we can't step down 2 stairs coz index will be -1(a[1]->a[0]->a[-1])
-//			right=func(index-2,arr)+Math.abs(arr[index]-arr[index-2]);
-//		return Math.min(left,right);
-//	}
-//
-//	public static void main(String[] args)
-//	{
-//		int n=6;
-//		int arr[]= {30,10,60,10,60,50};
-//		System.out.println("Minimum energy required is: "+func(n-1,arr));
-//	}
-//}
+public class frogjump 
+{
+	public static int func(int index,int arr[])
+	{
+		if(index==0)		//if frog is at 0th index, then min energy required is 0		
+			return 0;
+		int left=func(index-1,arr)+Math.abs(arr[index]-arr[index-1]);
+		int right=Integer.MAX_VALUE;
+		if(index>1)			//if we are at index 1, then we can't step down 2 stairs coz index will be -1(a[1]->a[0]->a[-1])
+			right=func(index-2,arr)+Math.abs(arr[index]-arr[index-2]);
+		return Math.min(left,right);
+	}
+	public static void main(String[] args)
+	{
+		int n=6;
+		int arr[]= {30,10,60,10,60,50};
+		System.out.println("Minimum energy required is: "+func(n-1,arr));
+	}
+}
 
 
 //Memoization(DP Solution)       converting recursive sol to memoization concept gives DP solution
-//public class frogjump 
-//{
-//	public static int func(int index,int arr[],int dp[])
-//	{
-//		if(index==0)		//if frog is at 0th index, then min energy required is 0		
-//			return 0;
-//		if(dp[index]!=-1)
-//			return dp[index];
-//		int left=func(index-1,arr,dp)+Math.abs(arr[index]-arr[index-1]);
-//		int right=Integer.MAX_VALUE;
-//		if(index>1)			//if we are at index 1, then we can't step down 2 stairs coz index will be -1(a[1]->a[0]->a[-1])
-//			right=func(index-2,arr,dp)+Math.abs(arr[index]-arr[index-2]);
-//		return dp[index]=Math.min(left,right);
-//	}
-//
-//	public static void main(String[] args)
-//	{
-//		int n=6;
-//		int arr[]= {30,10,60,10,60,50};
-//		int dp[]=new int[n+1];
-//		Arrays.fill(dp, -1);
-//		System.out.println("Minimum energy required is: "+func(n-1,arr,dp));
-//	}
-//}
+public class frogjump 
+{
+	public static int func(int index,int arr[],int dp[])
+	{
+		if(index==0)		//if frog is at 0th index, then min energy required is 0		
+			return 0;
+		if(dp[index]!=-1)
+			return dp[index];
+		int left=func(index-1,arr,dp)+Math.abs(arr[index]-arr[index-1]);
+		int right=Integer.MAX_VALUE;
+		if(index>1)			//if we are at index 1, then we can't step down 2 stairs coz index will be -1(a[1]->a[0]->a[-1])
+			right=func(index-2,arr,dp)+Math.abs(arr[index]-arr[index-2]);
+		return dp[index]=Math.min(left,right);
+	}
+	public static void main(String[] args)
+	{
+		int n=6;
+		int arr[]= {30,10,60,10,60,50};
+		int dp[]=new int[n+1];
+		Arrays.fill(dp, -1);
+		System.out.println("Minimum energy required is: "+func(n-1,arr,dp));
+	}
+}
 
 
-//tabulation(bottom-up approach) space complexity is O(n), when compared with recursion coz there is no stack space as we're using dp[] array
-//public class frogjump 
-//{
-//	public static void main(String[] args)
-//	{
-//		int n=6;
-//		int arr[]= {30,10,60,10,60,50};
-//		int dp[]=new int[n+1];
-//		Arrays.fill(dp, -1);
-//		dp[0]=0;			//ex of bottom-up approach, first setting the 0th index, and then looping from 1st index
-//		for(int index=1;index<arr.length;index++)
-//		{
-//			int firststep=dp[index-1]+Math.abs(arr[index]-arr[index-1]);
-//			int secondstep=Integer.MAX_VALUE;
-//			if(index>1)			
-//				secondstep=dp[index-2]+Math.abs(arr[index]-arr[index-2]);
-//			dp[index]=Math.min(firststep,secondstep);
-//		}
-//		System.out.println("Minimum energy required is: "+dp[n-1]);
-//	}
-//}
+//Tabulation(bottom-up approach) space complexity is O(n), when compared with recursion coz there is no stack space as we're using dp[] array
+public class frogjump 
+{
+	public static void main(String[] args)
+	{
+		int n=6;
+		int arr[]= {30,10,60,10,60,50};
+		int dp[]=new int[n+1];
+		Arrays.fill(dp, -1);
+		dp[0]=0;			//ex of bottom-up approach, first setting the 0th index, and then looping from 1st index
+		for(int index=1;index<arr.length;index++)
+		{
+			int firststep=dp[index-1]+Math.abs(arr[index]-arr[index-1]);
+			int secondstep=Integer.MAX_VALUE;
+			if(index>1)			
+				secondstep=dp[index-2]+Math.abs(arr[index]-arr[index-2]);
+			dp[index]=Math.min(firststep,secondstep);
+		}
+		System.out.println("Minimum energy required is: "+dp[n-1]);
+	}
+}
 
 
 //space optimisation, dp[] array is also removed to reduce the space 
