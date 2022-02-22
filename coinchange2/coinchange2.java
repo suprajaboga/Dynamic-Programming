5fcb1a4e57a4b1c1680b799c52ab5fb1971cc9ba
suprajaboga <97597175+suprajaboga@users.noreply.github.com>
2022-02-22 14:30:59 +0530

    File updated

--- coinchange2/coinchange2.java
+++ coinchange2/coinchange2.java
@@ -3,32 +3,32 @@
 package coinchange2;
 
 //Recursion		TC : O(greater than 2 pow n), coz we're staying at the same index if we pick an element		SC : O(recursion stack space)
-//public class coinchange2
-//{
-//	public static int print(int index,int amount,int coins[])
-//	{
-//		if(index==0)
-//		{
-//			if(amount%coins[0]==0)
-//				return 1;
-//			else
-//				return 0;
-//		}
-//		int nottake,take=0;
-//		nottake = print(index-1,amount,coins);
-//		if(coins[index]<=amount)
-//			take = print(index,amount-coins[index],coins);
-//		return nottake + take;
-//	}
-//
-//	public static void main(String[] args)
-//	{
-//		int coins[]= {1,2,3};
-//		int amount=4;
-//		int n=coins.length;
-//		System.out.println("Total no.of ways are: "+print(n-1,amount,coins));
-//	}
-//}
+public class coinchange2
+{
+	public static int print(int index,int amount,int coins[])
+	{
+		if(index==0)
+		{
+			if(amount%coins[0]==0)
+				return 1;
+			else
+				return 0;
+		}
+		int nottake,take=0;
+		nottake = print(index-1,amount,coins);
+		if(coins[index]<=amount)
+			take = print(index,amount-coins[index],coins);
+		return nottake + take;
+	}
+
+	public static void main(String[] args)
+	{
+		int coins[]= {1,2,3};
+		int amount=4;
+		int n=coins.length;
+		System.out.println("Total no.of ways are: "+print(n-1,amount,coins));
+	}
+}
 
 
 //Tabulation		TC : O(n*target)		SC : O(n*target)
@@ -64,34 +64,34 @@ public class coinchange2
 
 
 //Space optimisation		TC : O(n*target)		SC : O(target)
-//public class coinchange2
-//{
-//	public static void main(String[] args)
-//	{
-//		int coins[]= {1,2,5};
-//		int target=5;
-//		int n=coins.length;
-//		int prev[]=new int[target+1];
-//		int curr[]=new int[target+1];
-//		for(int i=0;i<=target;i++)
-//		{
-//			if(i%coins[0]==0)
-//				prev[i]=1;
-//			else
-//				prev[i]=0;
-//		}
-//		for(int index=1;index<n;index++)
-//		{
-//			for(int sum=0;sum<=target;sum++)
-//			{
-//				int nottake,take=0;
-//				nottake = prev[sum];
-//				if(coins[index]<=sum)
-//					take = curr[sum-coins[index]];
-//				curr[sum]= nottake + take;
-//			}
-//			prev=curr;
-//		}
-//		System.out.println("Total no.of ways are: "+prev[target]);
-//	}
-//}
+public class coinchange2
+{
+	public static void main(String[] args)
+	{
+		int coins[]= {1,2,5};
+		int target=5;
+		int n=coins.length;
+		int prev[]=new int[target+1];
+		int curr[]=new int[target+1];
+		for(int i=0;i<=target;i++)
+		{
+			if(i%coins[0]==0)
+				prev[i]=1;
+			else
+				prev[i]=0;
+		}
+		for(int index=1;index<n;index++)
+		{
+			for(int sum=0;sum<=target;sum++)
+			{
+				int nottake,take=0;
+				nottake = prev[sum];
+				if(coins[index]<=sum)
+					take = curr[sum-coins[index]];
+				curr[sum]= nottake + take;
+			}
+			prev=curr;
+		}
+		System.out.println("Total no.of ways are: "+prev[target]);
+	}
+}
